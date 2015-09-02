af103d72a458601cf917a2b0f526a8a1bb0a1534
Blake Smith <blakesmithphoto@gmail.com>
2015-09-01 18:11:14 -0700

    First build with PHP form validations

---
+++ script.php
@@ -0,0 +1,31 @@
+<?php
+
+$test = '';
+
+include('index.html');
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+	// Start with Form Validation
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$submit = $_POST['submit'];
+
+	if (isset($name, $email, $password)) { 
+
+		echo '<div class="row">
+				<div class="col-md-3"></div>
+				<div class="col-md-6">
+				<p>Hey ' . $name . '! Thanks for creating an account, we will send you a confirmation email to:
+				 ' . $email . '</p></div></div><div class="col-md-3"</div>';
+
+	}
+
+
+} // Close the main server request method POST
+
+
+?>
